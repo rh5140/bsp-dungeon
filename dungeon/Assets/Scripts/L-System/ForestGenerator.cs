93524a285921a1894069960452536a363cf596ec
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2026-02-09 00:12:38 -0800

    Quick forest generation
    Just pure random

--- dungeon/Assets/Scripts/L-System/ForestGenerator.cs
+++ dungeon/Assets/Scripts/L-System/ForestGenerator.cs
@@ -5,22 +5,36 @@ public class ForestGenerator : MonoBehaviour
     [Header("Spawn Range")]
     public float xMin = 1;
     public float xMax = 199;
-    public float yMin = 1;
-    public float yMax = 199;
+    public float zMin = 1;
+    public float zMax = 199;
 
     [Header("Prefab Processing")]
-    public GameObject treeToSpawn;
+    public int numTrees = 50;
+    public GameObject[] treeToSpawn;
     public float rollMin = -25f;
-    public float rollMax = 0f;
+    public float rollMax = 25f;
     
     void Start()
     {
-        
+        SpawnTrees();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnTrees()
     {
-        
+        for (int i = 0; i < numTrees; i++)
+        {
+            GameObject newTree = Instantiate(ReturnRandomPrefab(), new Vector3(GetRandomPoint(xMin, xMax), 0, GetRandomPoint(zMin, zMax)), Quaternion.identity);
+            newTree.transform.rotation *= Quaternion.Euler(0, 0, GetRandomPoint(rollMin, rollMax)); 
+        }
+    }
+
+    GameObject ReturnRandomPrefab()
+    {
+        return treeToSpawn[Random.Range(0,treeToSpawn.Length)];
+    }
+
+    float GetRandomPoint(float min, float max)
+    {
+        return Random.Range(min, max);
     }
 }
